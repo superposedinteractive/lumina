622f846189dc32c3c5ad3f60464903e03b38d0b7
japan <49401941+japannt@users.noreply.github.com>
2023-03-26 15:00:02 +0200

    allow making the button a ghost

--- scripts/ent/block/button.gd
+++ scripts/ent/block/button.gd
@@ -2,10 +2,18 @@ extends lumina_entity
 
 @export var target : lumina_entity
 @export var once : bool = false
+@export var solid_to_player : bool = true
 
 func _ready():
 	if !target is lumina_entity:
 		util.notif(str(self) + " Target isn't a lumina_entity or doesn't exist.")
+
+	if solid_to_player:
+		get_node("button").collision_layer = 1
+		get_node("button").collision_mask = 1
+	else:
+		get_node("button").collision_layer = 2
+		get_node("button").collision_mask = 2
 
 func use():
 	if !target is lumina_entity:
